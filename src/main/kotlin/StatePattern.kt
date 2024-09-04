1e4554a7bcef9689b7a3446636cc5daacbb71ede
Mehmet Ali Öyür <mehmetalioyur@gmail.com>
2024-09-04 20:32:58 +0300

    state based with enum class and extension user manage

--- src/main/kotlin/StatePattern.kt
+++ src/main/kotlin/StatePattern.kt
@@ -2,39 +2,36 @@ package org.example
 
 fun main() {}
 enum class UserState(
-    val signUp: (User, String) -> Unit,
-    val verifyEmail: (User, String) -> Unit,
+    val signUp: User.(String) -> Unit,
+    val verifyEmail: User.(String) -> Unit,
     val viewContent: () -> Unit,
-    val viewProfile: (User) -> Unit,
-    val editProfile: (User, String) -> Unit,
+    val viewProfile: User.() -> Unit,
+    val editProfile: User. (String) -> Unit,
 ) {
     ANONYMOUS(
-        signUp = { user, email -> println("Signing up with email: $email"); user.email = email; user.state = UNVERIFIED },
-        verifyEmail = { _, _ -> println("You must sign up before verifying your email.") },
+        signUp = { email -> println("Signing up with email: $email"); this.email = email; this.state = UNVERIFIED },
+        verifyEmail = { println("You must sign up before verifying your email.") },
         viewContent = { println("Viewing public content.") },
         viewProfile = { println("You must sign in to view your profile.") },
-        editProfile = { _, _ -> println("You must sign in to edit your profile") }
+        editProfile = { println("You must sign in to edit your profile") }
     ),
     UNVERIFIED(
-        signUp = { _, _ -> println("You are already signed up.") },
-        verifyEmail = { user, token -> println("Verifying email with token: $token"); user.state = AUTHENTICATED },
+        signUp = { println("You are already signed up.") },
+        verifyEmail = { println("Verifying email with token: $this.token"); state = AUTHENTICATED },
         viewContent = { println("Viewing personalized content for unverified account.") },
-        viewProfile = { user -> println("Profile: ${user.email} (Unverified account, please verify your email).") },
-        editProfile = { _, _ -> println("Please verify your account before editing your profile.") }
+        viewProfile = { println("Profile: ${this.email} (Unverified account, please verify your email).") },
+        editProfile = { println("Please verify your account before editing your profile.") }
     ),
 
     AUTHENTICATED(
-        signUp = { _, _ -> println("You are already signed up and authenticated.") },
-        verifyEmail = { _, _ -> println("You are already verified.") },
+        signUp = { println("You are already signed up and authenticated.") },
+        verifyEmail = { println("You are already verified.") },
         viewContent = { println("Viewing personalized content.") },
-        viewProfile = { user -> println("Profile: ${user.email} (Fully authenticated).") },
-        editProfile = { user, newEmail -> println("Profile: Updating email from ${user.email} to $newEmail.");user.email = newEmail }
+        viewProfile = { println("Profile: ${this.email} (Fully authenticated).") },
+        editProfile = { println("Profile: Updating email from ${this.email} to $it..");this.email = it }
     )
 }
 
-
-
-// CONTEXT
 class User(var email: String? = null, var state: UserState = UserState.ANONYMOUS) {
     fun signUp(email: String) = state.signUp(this, email)
     fun verifyEmail(token: String) = state.verifyEmail(this, token)
